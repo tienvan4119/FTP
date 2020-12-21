137a9093001573500fcb73f9e30fed3c4ead2bdd
tienvan <vannt808@gmail.com>
2020-12-22 04:09:59 +0700

    'txtpath'

--- src/Client.java
+++ src/Client.java
@@ -1,14 +1,41 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class Client {
+public class Client extends Component {
     private JTextField txtUsername;
     private JTextField txtHost;
     private JTextField txtPassword;
     private JTextField txtPort;
     private JPanel panelMain;
     private JButton btnLogin;
-    private JTextArea textArea1;
-    private JTextArea textArea2;
+    private JButton uploadButton;
+    private JTree tree2;
+    private JTextField txtPath;
+    private JButton downloadButton;
+
+    public Client() {
+        uploadButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fc = new JFileChooser();
+                fc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                int returnVal = fc.showOpenDialog(Client.this);
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fc.getSelectedFile();
+
+                    //This is where a real application would open the file.
+                    System.out.println("Opening: " + file.getAbsolutePath());
+                    txtPath.setText(file.getAbsolutePath());
+                } else {
+                    System.out.println("Open command cancelled by user.");
+                }
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Client");
